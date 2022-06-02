1b2d76fd44479316700c4d31325d6cc798fe79b4
Matheba <itumeleng.matheba@vcontractor.co.za>
2022-06-02 04:24:49 +0200

    fix middleware

--- app.js
+++ app.js
@@ -7,98 +7,102 @@ const { verify } = require("./helpers/middleware");
 
 const app = express();
 app.use(express.json());
-app.use(verify);
-// let initalValues = [
-//   {
-//     id: 1,
-//     food: "Glazed oysters with zucchini pearls and sevruga caviar",
-//     winePairing: "kleine zalze brut rose",
-//   },
-//   {
-//     id: 2,
-//     food: "Bream with asparagus, tempura mussels and a lime velouté",
-//     winePairing: "red city blend",
-//   },
-//   {
-//     id: 3,
-//     food: "Seared anhi tuna with provincial vegetables and tapenade croutons",
-//     winePairing: "bizoe semillon",
-//   },
+ app.use(verify);
+let menu = [
+  {
+    id: 1,
+    food: "Glazed oysters with zucchini pearls and sevruga caviar",
+    winePairing: "kleine zalze brut rose",
+  },
+  {
+    id: 2,
+    food: "Bream with asparagus, tempura mussels and a lime velouté",
+    winePairing: "red city blend",
+  },
+  {
+    id: 3,
+    food: "Seared anhi tuna with provincial vegetables and tapenade croutons",
+    winePairing: "bizoe semillon",
+  },
 
-//   {
-//     id: 4,
-//     food: "Aged gouda with espresso, hazelnut and onion",
-//     winePairing: "thelema early harvest",
-//   },
+  {
+    id: 4,
+    food: "Aged gouda with espresso, hazelnut and onion",
+    winePairing: "thelema early harvest",
+  },
 
-//   {
-//     id: 5,
-//     food: "Dark chocolate panna cotta with a rhubarb and cherry compote",
-//     winePairing: "clarington sauvignon blanc",
-//   },
+  {
+    id: 5,
+    food: "Dark chocolate panna cotta with a rhubarb and cherry compote",
+    winePairing: "clarington sauvignon blanc",
+  },
 
-//   { price: 100 },
-// ];
+  
+];
+ const pricePerPerson = {price: 100} 
+ let orders = [];
 
-// let orders = [];
+app.post("/order", (req, res) => {
+  const order = req.body;
+  orders.push(order);
+  console.log(orders);
 
-// app.post("/order", (req, res) => {
-//   const order = req.body;
-//   orders.push(order);
-//   console.log(orders);
+  res.send("Added order to order table");
+});
 
-//   res.send("Added order to order table");
-// });
+app.get("/menu", (req, res) => {
+  res.send(menu);
+});
 
-// app.get("/initialize", (req, res) => {
-//   res.send(initalValues);
-// });
+app.get("/price", (req, res) => {
+  res.send(pricePerPerson);
+});
 
-// app.get("/menu/:id", (req, res) => {
-//   const id = req.params.id;
-//   console.log(id);
-//   for (let item of menu) {
-//     console.log(item.id);
-//     if (item.id == id) {
-//       res.json(item);
-//       return;
-//     }
-//   }
-//   res.status(404).send("item not found");
-// });
+app.get("/menu/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  for (let item of menu) {
+    console.log(item.id);
+    if (item.id == id) {
+      res.json(item);
+      return;
+    }
+  }
+  res.status(404).send("item not found");
+});
 
-// app.put("/menu/:id", (req, res) => {
-//   const id = req.params.id;
-//   const newWine = "thelema late harvest";
+app.put("/menu/:id", (req, res) => {
+  const id = req.params.id;
+  const newWine = "thelema late harvest";
 
-//   for (let i = 0; i < menu.length; i++) {
-//     let menuItem = menu[i];
-//     console.log(menuItem);
-//     if (menuItem.id == id) {
-//       menu[i].winePairing = newWine;
-//       console.log(menu[i].winePairing);
-//     }
-//   }
-//   res.send("menu Item is edited");
-// });
+  for (let i = 0; i < menu.length; i++) {
+    let menuItem = menu[i];
+    console.log(menuItem);
+    if (menuItem.id == id) {
+      menu[i].winePairing = newWine;
+      console.log(menu[i].winePairing);
+    }
+  }
+  res.send("menu Item is edited");
+});
 
-// app.delete("/menu/:id", (req, res) => {
-//   const id = req.params.id;
+app.delete("/menu/:id", (req, res) => {
+  const id = req.params.id;
 
-//   menu = menu.filter((i) => {
-//     if (i.id != id) {
-//       return true;
-//     }
-//     return false;
-//   });
-//   res.send("Menu item is deleted");
-// });
+  menu = menu.filter((i) => {
+    if (i.id != id) {
+      return true;
+    }
+    return false;
+  });
+  res.send("Menu item is deleted");
+});
 
-// app.post("/order", (req, res) => {
-//   const order = req.body;
-//   orders.push(order);
-//   res.send("Added order to order table");
-// });
+app.post("/order", (req, res) => {
+  const order = req.body;
+  orders.push(order);
+  res.send("Added order to order table");
+});
 
 const baseUrl = process.env.BASE_URL;
 
