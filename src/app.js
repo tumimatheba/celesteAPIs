f3f23bff8655c6f390b125a6a23120262d89bd80
tumimatheba <tumimatheba@gmail.com>
2022-06-23 15:28:56 +0200

    fix payment

--- src/app.js
+++ src/app.js
@@ -103,7 +103,8 @@ app.post("/order", (req, res) => {
   orders.push(order);
   res.send("Added order to order table");
 });
- app.use(verify);
+
+ //app.use(verify);
 const baseUrl = process.env.BASE_URL;
 
 
