3c30c9b7af969095b9ebbb9cde959f3b747ae52e
tumimatheba <tumimatheba@gmail.com>
2022-06-24 07:29:51 +0200

    debug payment

--- src/app.js
+++ src/app.js
@@ -116,7 +116,7 @@ app.post("/auth",  (req, res) => {
     authCode,
   });
 
-  // const tokenURL = 'https://vodapay-gateway.sandbox.vfs.africa/v2/authorizations/applyTokenSigned';
+   const tokenURL = 'https://vodapay-gateway.sandbox.vfs.africa/v2/authorizations/applyToken';
 
   // const accessTokenResponse = await frontEndRequest(data, tokenURL);
   
