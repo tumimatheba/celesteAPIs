d15f556343260c683721598b82819f5929edb1cd
tumimatheba <tumimatheba@gmail.com>
2022-06-29 13:04:50 +0200

    fix signature

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,23 +2,23 @@ require("dotenv").config();
 const axios = require("axios");
 const { DateTime } = require("luxon");
 const crypto = require('crypto')
-const  { createSignature } = require("../src/signature")
-// const fs = require('fs')
+// const  { createSignature } = require("../src/signature")
+ const fs = require('fs')
  const clientId = process.env.CLIENT_ID;
 
-// const createSignature = ({ uriPath, clientId, requestTime, requestBody }) => {
-//   const privateKeyText = fs.readFileSync('src/certs/rsa_private_key.pem', 'utf8')
-//   const unsignedContent = `POST ${uriPath}\n${clientId}.${requestTime}.${JSON.stringify(
-//     requestBody
-//   )}`;
+const createSignature = ({ uriPath, clientId, requestTime, requestBody }) => {
+  const privateKeyText = fs.readFileSync('src/certs/rsa_private_key.pem', 'utf8')
+  const unsignedContent = `POST ${uriPath}\n${clientId}.${requestTime}.${JSON.stringify(
+    requestBody
+  )}`;
 
-//   const privateKey = crypto.createPrivateKey(privateKeyText, 'utf8');
-//   const signer = crypto.createSign('RSA-SHA256');
-//   signer.write(unsignedContent);
-//   signer.end();
-//   let signed = signer.sign(privateKey, 'base64');
-//   return signed
-// }
+  const privateKey = crypto.createPrivateKey(privateKeyText, 'utf8');
+  const signer = crypto.createSign('RSA-SHA256');
+  signer.write(unsignedContent);
+  signer.end();
+  let signed = signer.sign(privateKey, 'base64');
+  return signed
+}
 
 const vodaPayRequest = async (requestBody, path) => {
   const uriPath = new URL(path).pathname;
