ae52185824d0020e527ced5fae8f9c85bfce5b7a
tumimatheba <tumimatheba@gmail.com>
2022-06-29 12:49:07 +0200

    refactor code

--- src/signature.js
+++ src/signature.js
@@ -4,14 +4,9 @@ const fs = require('fs')
 
 const createSignature = ({ uriPath, clientId, requestTime, requestBody }) => {
   const privateKeyText = process.env.PRIVATE_KEY;
-  // const privateKeyTexts = fs.readFileSync('src/certs/rsa_private_key.pem', 'utf8')
- 
-  console.log(privateKeyText);
- 
   const unsignedContent = `POST ${uriPath}\n${clientId}.${requestTime}.${JSON.stringify(
     requestBody
   )}`;
-  createSignature();
   const privateKey = crypto.createPrivateKey(privateKeyText, 'utf8');
   const signer = crypto.createSign('RSA-SHA256');
   signer.write(unsignedContent);
